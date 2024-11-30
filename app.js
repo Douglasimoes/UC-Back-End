499dc0e7a0963480ee9926adeff6540b0761c1c0
Douglas <douglasskt@hotmail.com>
2024-11-29 22:11:13 -0300

    Aula de Insomnia

--- app.js
+++ app.js
@@ -10,8 +10,28 @@ app.get( '/', (requisicao, resposta) => {
     resposta.status(200).send(carros2024)
 });
 
+app.get('/:sigla', (requisicao, resposta) => {
+    const siglaInformada = requisicao.params.sigla.toUpperCase(); //Obtendo a Sigla
+    const carro = carros2024.find((infoCarro) => infoCarro.sigla === siglaInformada); //Busca o carro pela sigla
+        if (!carro) {
+            // ! Vazio Not
+             resposta
+            .status(404)
+            .send(
+                'Não existe um carro com a sigla informada!' //Mensagem de erro
+             );
+         return;    
+    }
+    resposta.status(200).send(carro); //Se encontrado a sigla retorna a resposta correta
+})
+    app.post('/', (req, res) => {
+        const novoCarro = req.body; //Obtem o corpo enviado para incluir um carro
+        carros2024.push(novoCarro); //Adiciona o novo carro na lista de carros
+        res.status(200).send(novoCarro); //Retorna o carro adicionado com status 200 (Ok)
+    });
+
 // Inicia o servidor na porta 3000:
-app.listen(3000,() => console.log("Servidor Rodando com Sucesso"));
+app.listen(3001,() => console.log("Servidor Rodando com Sucesso"));
 
 // node app.js
 // localhost:3000/
--- tabelacarros.js
+++ tabelacarros.js
@@ -18,6 +18,62 @@ const carros2024 = [
         aceleracao: 2.8,
         preco: 330000,
     },
+    {
+        nome: 'BMW 320I',
+        sigla: 'BMW',
+        velocidadeMaxima: 200,
+        potencia: 500,
+        consumo: 7.5,
+        aceleracao: 4.0,
+        preco: 120000,
+    },
+    {
+        nome: 'Bugatti',
+        sigla: 'BGT',
+        velocidadeMaxima: 420,
+        potencia: 1500,
+        consumo: 4.5,
+        aceleracao: 2.5,
+        preco: 20000000,
+    },
+    {
+        nome: 'Audi R8',
+        sigla: 'AUD',
+        velocidadeMaxima: 330,
+        potencia: 610,
+        consumo: 8.3,
+        aceleracao: 6.0,
+        preco: 400000,
+    },
+    {
+        nome: 'Porsche',
+        sigla: 'POR',
+        velocidadeMaxima: 300,
+        potencia: 570,
+        consumo: 9.2,
+        aceleracao: 8.8,
+        preco: 430000,
+    },
+    {
+        nome: 'Nissan GTR',
+        sigla: 'GTR',
+        velocidadeMaxima: 305,
+        potencia: 670,
+        consumo: 7.2,
+        aceleracao: 5.6,
+        preco: 830000,
+    },
+    {
+        nome: 'Mazda RX8',
+        sigla: 'RX8',
+        velocidadeMaxima: 290,
+        potencia: 480,
+        consumo: 10.5,
+        aceleracao: 10.1,
+        preco: 335000,
+    },
+    
+
 ];
 
 export default carros2024;
